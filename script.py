303a3aea11e5122d794dc8935bee9317994d39a0
marcldl <marcldl>
2005-06-07 02:55:41 +0000

    Figuring out some event structure.

--- script.py
+++ script.py
@@ -12,3 +12,20 @@ def dothebartmanPart(event):
 
 def onInput(event):
     event.window.write(event.text)
+    
+def onStart(event):
+    # FIXME, find a list of networks to join from somewhere, prolly conf
+    #         then join them
+    def list_of_networks_to_join_from_somewhere():
+        return ["ANet", "BNet", "CNet"]
+    
+    on_start_networks = list_of_networks_to_join_from_somewhere()
+    
+    for network in on_start_networks:
+        # FIXME, connect to it
+        #        how do we do this?
+        #        we prolly call something.connect(network)
+        #        or else maybe we create a network object and call
+        #        network_object.connect()
+        
+        print "Connecting to %s" % network
