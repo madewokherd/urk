f90b548ec64a9e4fe74635781ecbb3b8abb16259
marcldl <marcldl>
2005-07-05 18:20:53 +0000

    Fix some ui.tabs stuff.

--- urk.py
+++ urk.py
@@ -5,14 +5,15 @@ import ui
 get_window = {}
 
 def connect(network):
-    if ui.ui.tabs.get_n_pages() == 1 and not ui.ui.tabs.get_nth_page(0).get_data("network"):
-        network_window = ui.ui.tabs.get_nth_page(0)
-        network_window.set_data('network', network)
-        ui.ui.tabs.set_tab_label_text(network_window, network.server)
+    if ui.tabs.get_nth_page(0).type == "first_window":
+        network_window = ui.tabs.get_nth_page(0)
+        ui.tabs.set_tab_label_text(network_window, network.server)
     else:
         network_window = ui.IrcWindow(network.server)
-        network_window.type = "status"
-        ui.new_tab(network_window, network) 
+        ui.new_tab(network_window, network)
+
+    network_window.network = network  
+    network_window.type = "status" 
            
     ui.activate(network_window)
     
