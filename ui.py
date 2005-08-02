c425baf3221e5e555558278cfae5a7ade97fd4bb
marcldl <marcldl>
2005-08-02 18:16:05 +0000

    Cleanup, make tabs iterable

--- ui.py
+++ ui.py
@@ -1,21 +1,15 @@
 import time
 
-import pygtk
 import gtk
-import gobject
 import pango
 
 import conf
 import events
-#import theme
 import parse_mirc
-
-modifiers = (gtk.gdk.CONTROL_MASK, gtk.gdk.MOD1_MASK,
-                gtk.gdk.MOD2_MASK, gtk.gdk.MOD3_MASK,
-                gtk.gdk.MOD4_MASK, gtk.gdk.MOD5_MASK)
-                        
+          
 MOD_MASK = 0
-for m in modifiers:
+for m in (gtk.gdk.CONTROL_MASK, gtk.gdk.MOD1_MASK, gtk.gdk.MOD3_MASK,
+             gtk.gdk.MOD4_MASK, gtk.gdk.MOD5_MASK):
     MOD_MASK |= m   
 
 # FIXME: get rid of this
@@ -35,18 +29,18 @@ def urk_about(action):
     
     about.show_all()
     
-def get_tab_actions(tabs, page_num):
+def get_tab_actions(page_num):
     def close_tab(action):
-        ui.tabs.remove_page(page_num)
+        tabs.remove_page(page_num)
         
     to_add = (
         ("CloseTab", gtk.STOCK_CLOSE, "_Close Tab", None, None, close_tab),
         )
         
-    actions = gtk.ActionGroup("Tab")
-    actions.add_actions(to_add)
+    tab_actions = gtk.ActionGroup("Tab")
+    tab_actions.add_actions(to_add)
     
-    return actions
+    return tab_actions
     
 def get_urk_actions(ui):
     def connectToArlottOrg(widget):
@@ -232,39 +226,26 @@ class IrcChannelWindow(IrcWindow):
         win.pack2(self.nicklist, resize=False)
         
         return win
-
+        
+class IrcTabs(gtk.Notebook):
+    def __init__(self):
+        gtk.Notebook.__init__(self)
+        
+        self.set_property("tab-pos", gtk.POS_TOP)
+        self.set_border_width(10)          
+        self.set_scrollable(True)
+        self.set_show_border(True)
+    
+    # FIXME: remove this when pygtk2.8 comes around
+    def __iter__(self):
+        return iter(self.get_children())
+        
 class IrcUI(gtk.Window):
     def shutdown(self, *args):
         conf.set("xy", self.get_position())
         conf.set("wh", self.get_size())
         
         enqueue(quit)
-        
-    def make_notebook(self, ui):
-        self.tabs = gtk.Notebook()
-        self.tabs.set_property("tab-pos", gtk.POS_TOP)
-        self.tabs.set_border_width(10)          
-        self.tabs.set_scrollable(True)
-        self.tabs.set_show_border(True)
-
-        def tab_popup(widget, event):
-            if event.button == 3: # right click
-                page_num = self.tabs.page_num(widget.child_window)
-                
-                # add some tab UI                
-                tab_id = self.ui_manager.add_ui_from_file("tabui.xml")
-                self.ui_manager.insert_action_group(get_tab_actions(self.tabs, page_num), 0)
-
-                menu = self.ui_manager.get_widget("/TabPopup")
-                
-                # remove the tab UI, so we can recompute it later
-                def remove_tab_ui(action):
-                    self.ui_manager.remove_ui(tab_id)
-                menu.connect("deactivate", remove_tab_ui)
-
-                menu.popup(None, None, None, event.button, event.time)
-
-        self.tabs.tab_popup = tab_popup
 
     def __init__(self):
         # threading stuff
@@ -280,65 +261,82 @@ class IrcUI(gtk.Window):
         self.move(*xy)
         self.set_default_size(*wh)
         
-        # set up ui manager stuff     
-        self.ui_manager = gtk.UIManager()
-        self.add_accel_group(self.ui_manager.get_accel_group())
-        self.ui_manager.add_ui_from_file("ui.xml")
-        self.ui_manager.insert_action_group(get_urk_actions(self), 1)
+        #self.add_accel_group(ui_manager.get_accel_group())
+        ui_manager.add_ui_from_file("ui.xml")
+        ui_manager.insert_action_group(get_urk_actions(self), 0)
         
-        # tabs        
-        self.make_notebook(self.ui_manager)
-        
+        menu = ui_manager.get_widget("/MenuBar")
+
         # widgets
         box = gtk.VBox(False)
-        box.pack_start(self.ui_manager.get_widget("/MenuBar"), expand=False)
-        box.pack_end(self.tabs)
+        box.pack_start(menu, expand=False)
+        box.pack_end(tabs)
         
         self.connect("delete_event", self.shutdown)
 
         self.add(box)
         self.show_all()
         
-def fix_tab_label(window):
+# Add a tab label with this window's title
+def add_tab_label(window):
+    def tab_popup(widget, event):
+        if event.button == 3: # right click
+            page_num = tabs.page_num(widget.child_window)
+            
+            # add some tab UI                
+            tab_id = ui_manager.add_ui_from_file("tabui.xml")
+            ui_manager.insert_action_group(get_tab_actions(page_num), 0)
+
+            tab_menu = ui_manager.get_widget("/TabPopup")
+            
+            # remove the tab UI, so we can recompute it later
+            def remove_tab_ui(action):
+                ui_manager.remove_ui(tab_id)
+            tab_menu.connect("deactivate", remove_tab_ui)
+
+            tab_menu.popup(None, None, None, event.button, event.time)
+
     label = gtk.EventBox()        
     label.add(gtk.Label(window.title))
     label.child_window = window
-    label.connect("button-press-event", ui.tabs.tab_popup)
+    label.connect("button-press-event", tab_popup)
     label.show_all()
 
-    ui.tabs.set_tab_label(window, label)
-        
+    tabs.set_tab_label(window, label)
+
+# Make a new tab with a window widget, optionally associate it with a network        
 def new_tab(window, network=None):
     window.network = network
-        
-    def focus_entry(*args):
-        window.entry.grab_focus()
-    window.connect("focus", focus_entry)
 
-    def new_tab_unsafe(window, network):
-        pos = ui.tabs.get_n_pages()
+    def new_tab_unsafe():
+        def focus_entry(*args):
+            window.entry.grab_focus()
+        window.connect("focus", focus_entry)
+    
+        pos = tabs.get_n_pages()
         
         if network:
             for i in reversed(xrange(pos)):
-                if ui.tabs.get_nth_page(i).network == network:
+                if tabs.get_nth_page(i).network == network:
                     pos = i+1
                     break
     
-        ui.tabs.insert_page(window, None, pos)
-        fix_tab_label(window)
+        tabs.insert_page(window, None, pos)
+        add_tab_label(window)
     
-    enqueue(new_tab_unsafe, window, network)
-        
-def activate(widget):
-    def to_activate(widget):
-        # if this is an actual widget, then we want its tab number
-        if not isinstance(widget, int):
-            widget = ui.tabs.page_num(widget)
-    
-        ui.tabs.set_current_page(widget)
-        ui.tabs.get_nth_page(widget).entry.grab_focus()
+    enqueue(new_tab_unsafe)
 
-    enqueue(to_activate, widget)
+# Select the page with the given window or with the given tab position
+def activate(window):
+    def to_activate(window):
+        # if this is an actual window, then we want its tab number
+        if not isinstance(window, int):
+            window = tabs.page_num(window)
+    
+        tabs.set_current_page(window)
+        tabs.get_nth_page(window).entry.grab_focus()
+
+    enqueue(to_activate, window)
 
 def get_window(target, src_event=None, src_name=''):
     if target.window:
@@ -362,8 +360,14 @@ class Quitting(Exception):
 def quit():
     raise Quitting
 
+# UI manager to use throughout   
+ui_manager = gtk.UIManager()
+    
+# build our tab widget
+tabs = IrcTabs()
+
+# build our overall UI
 ui = IrcUI()
-tabs = ui.tabs
 
 def start():
     first_window = IrcWindow("Status Window")
--- urk.py
+++ urk.py
@@ -5,26 +5,20 @@ import ui
 get_window = {}
 
 def connect(network):
-    if ui.tabs.get_n_pages() and ui.tabs.get_nth_page(0).type == "first_window":
-        window = ui.tabs.get_nth_page(0)
-        window.title = network.server
-        ui.fix_tab_label(window)
-
-    #FIXME: this is what I want to write
-    #for window in ui.tabs:
-    #    if window.type == "first_window":
-    #        window.title = network.server
-    #        ui.fix_tab_tabel(window)
-    #        break
+    for window in ui.tabs:
+        if window.type == "first_window":
+            window.title = network.server
+            ui.add_tab_label(window)
+            break
     else:
         window = ui.IrcWindow(network.server)
         ui.new_tab(window, network)
 
     window.network = network  
     window.type = "status" 
-           
+
     ui.activate(window)
-    
+
     get_window[network] = window
 
 if __name__ == "__main__":
