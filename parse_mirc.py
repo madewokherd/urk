23e748b9b7ede455652d3de96c05f0ca894fc508
marcldl <marcldl>
2005-08-28 02:03:41 +0000

    Implementations of parse_mirc for fun and stuff...

--- parse_mirc.py
+++ parse_mirc.py
@@ -13,14 +13,17 @@ colors = (
 
 def get_mirc_color(number):
     return colors[int(number) % len(colors)]
+    
+hexdigits = set('0123456789ABCDEF')
 
 def ishex(string):
-    for char in string:
-        if char.upper() not in '0123456789ABCDEF':
+    for char in string.upper():
+        if char not in hexdigits:
             return False
-    return True
+    
+    return bool(string)
 
-def parse_mirc(string):
+def parse_mirc1(string):
     start = 0
     pos = 0
     props = {}
@@ -127,52 +130,61 @@ def parse_mirc(string):
         tag_data.append((props.items(), start, len(new_string)))
     return tag_data, new_string
     
-def parse_mirc(string):
+def parse_mirc2(string):
     out, looking, tags, pos = "", {}, [], 0
+    
+    string += RESET
     
     while string:
         c = string[0]
 
         if c == MIRC_COLOR:
-            g, GET = {"f": "", "b": ""}, "f"
-        
-            next = string[1:]
- 
-            while next:
-                if next[0] in "0123456789" and len(g[GET]) <= 1:
-                    g[GET] += next[0]
-                    
-                    string = string[1:]
-                    
-                elif next[0] == ",":
-                    if not (g["f"] and next[1:] and next[1] in "0123456789"):
-                        break
-                        
-                    else:
-                        GET = "b"
-                        string = string[1:]
-                else:
-                    break
+            if c in looking:
+                tags += [looking[c] + [pos]]
+                del looking[c]
 
-                next = next[1:]
-                
-            fg, bg = g["f"], g["b"]
-     
-        elif c == BERS_COLOR:
             next = string[1:]
             
-            fg, bg = None, None 
+            if next[0:1] in "0123456789":
+                if next[1:2] in "0123456789":
+                    fg, next, string = get_mirc_color(next[0:2]), next[2:], string[2:]
+
+                else:
+                    fg, next, string = get_mirc_color(next[0:1]), next[1:], string[1:]
+                    
+                if next[0:1] == "," and next[1:2] in "0123456789":
+                    if next[2:3] in "0123456789":
+                        bg, string = get_mirc_color(next[1:3]), string[3:]
+
+                    else:
+                        bg, string = get_mirc_color(next[1:2]), string[2:]
+                        
+                    looking[c] = [[("foreground", fg), ("background", bg)], pos]
+                        
+                else:
+                    looking[c] = [[("foreground", fg)], pos]  
+
+        elif c == BERS_COLOR:
+            if c in looking:
+                tags += [looking[c] + [pos]]
+                del looking[c]
+        
+            next = string[1:]
             
             if ishex(next[0:6]):
                 fg = "#" + next[0:6]
                 
                 string = string[6:]
-                
-                if next[6:] and next[6] == ",":
-                    if ishex(next[7:13]):
-                        bg = "#" + next[7:13]
-                        
-                        string = string[7:]
+
+                if next[12:] and next[6] == "," and ishex(next[7:13]):
+                    bg = "#" + next[7:13]
+                    
+                    string = string[7:]
+
+                    looking[c] = [[("foreground", fg), ("background", bg)], pos]
+                    
+                else:
+                    looking[c] = [[("foreground", fg)], pos]
 
         elif c in (BOLD, UNDERLINE):
             if c in looking:
@@ -193,21 +205,105 @@ def parse_mirc(string):
             out += c
             pos += 1
             
-        if c in (MIRC_COLOR, BERS_COLOR):
-            if c in looking:
-                tags += [looking[c] + [pos]]
-                del looking[c]
-                
-            if fg:
-                if bg:
-                    looking[c] = [[("foreground", fg), ("background", bg)], pos]
-                else:
-                    looking[c] = [[("foreground", fg)], pos]
-            
         string = string[1:]
 
     return tags, out
         
+def parse_mirc3(string):
+    codes = MIRC_COLOR, BERS_COLOR, BOLD, UNDERLINE, RESET
+    tokill = {}
+
+    tags = dict([(c, []) for c in codes])
+    for i, s in enumerate(string + RESET):
+        if s in codes:
+            tokill[i] = 1
+        
+            tags[s] += [i]
+
+            if s == RESET:
+                for t in tags:
+                    tags[t] += [i]
+
+    ntags = []
+                
+    for p1, p2 in zip(tags[BOLD][::2], tags[BOLD][1::2]):
+        ntags.append(([("weight", BOLD)], p1, p2))
+        
+    for p1, p2 in zip(tags[UNDERLINE][::2], tags[UNDERLINE][1::2]):
+        ntags.append(([("underline", UNDERLINE)], p1, p2))
+
+    for p1, p2 in zip(tags[MIRC_COLOR], tags[MIRC_COLOR][1:]):
+        next = string[p1+1:p1+6]
+        
+        g, GET = {"f": "", "b": ""}, "f"
+
+        while next:
+            if next[0] in "0123456789" and len(g[GET]) <= 1:
+                g[GET] += next[0]
+                
+            elif next[0] == ",":
+                if not (g["f"] and next[1:] and next[1] in "0123456789"):
+                    break
+                    
+                else:
+                    GET = "b"
+            else:
+                break
+
+            next = next[1:]
+            
+        if g["f"]:
+            fg = get_mirc_color(g["f"])
+
+            if g["b"]:
+                bg = get_mirc_color(g["b"])
+
+                tokill[p1+len(g["f"] + g["b"])+1] = len(g["f"] + g["b"]) + 1
+                
+                ntags.append(([("foreground", fg), ("background", bg)], p1, p2))
+                
+            else:
+                tokill[p1+len(g["f"])] = len(g["f"])
+            
+                ntags.append(([("foreground", fg)], p1, p2))
+                            
+    for p1, p2 in zip(tags[BERS_COLOR], tags[BERS_COLOR][1:]):
+        next = string[p1+1:p1+14]
+
+        if next[5:] and ishex(next[0:6]):
+            fg, bg = "#" + next[0:6], ""
+            
+            if next[6:] and next[6] == ",":
+                if next[12:] and ishex(next[7:13]):
+                    bg = "#" + next[7:13]
+
+            if bg:
+                tokill[p1+13] = 13
+                
+                ntags.append(([("foreground", fg), ("background", bg)], p1, p2))
+            else:
+                tokill[p1+6] = 6
+                
+                ntags.append(([("foreground", fg)], p1, p2))
+
+    out = list(string + " ") 
+    for i in sorted(tokill, reverse=True):
+        del out[i-tokill[i]+1:i+1]
+    
+        for j, tag in enumerate(ntags):
+            p, fr, to = tag
+            
+            if i < fr:
+                fr -= tokill[i]
+            if i < to:
+                to -= tokill[i]
+                
+            ntags[j] = p, fr, to
+
+    return ntags, "".join(out)
+    
+parse_mirc = parse_mirc2
+
 if __name__ == "__main__":
     tests = [
         'not\x02bold\x02not',
@@ -221,9 +317,13 @@ if __name__ == "__main__":
         
         '\x04770077,FFFFFFbersirc color with background! \x04000077setting foreground! \x04reset!',
         
-        "\x03111Hello",
+        "\x03123Hello",
         
-        "\x0311,Hello"
+        "\x0312,Hello",
+        
+        "\x034Hello",
+        
+        "Bo\x02ld",
         ]
         
     results = [
@@ -237,7 +337,27 @@ if __name__ == "__main__":
         
         ([([('foreground', '#0000CC')], 0, 1), ([('foreground', '#0000CC')], 5, 6)], '<nick> text'),
         ([([('foreground', '#770077'), ('background', '#FFFFFF')], 0, 31), ([('foreground', '#000077'), ('background', '#FFFFFF')], 31, 51)], 'bersirc color with background! setting foreground! reset!'),
-        ]   
-    
+        ]
+        
+    #"""
+        
     for test in tests:
-        parse_mirc(test)
+        print parse_mirc2(test)
+
+    """
+
+    to_test = {
+        #1: parse_mirc1, 
+        2: parse_mirc2,
+        #3: parse_mirc3,
+        }
+    
+    r = range(10000)    
+    for i, f in to_test.items():
+        print "parse_mirc%s" % i 
+           
+        for i in r:
+            for test in tests:
+                f(test)
+                
+   #"""
