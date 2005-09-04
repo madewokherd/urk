4c0386a091304754531f17abb5c2b1ceb2049376
madewokherd <madewokherd>
2005-09-04 19:49:55 +0000

    updated conf_dummy to be like conf

--- conf_dummy.py
+++ conf_dummy.py
@@ -16,11 +16,33 @@ import weakref
 
 notify_objects = weakref.WeakKeyDictionary()
 
-def get(key):
-    return values.get(key)
+def get(key, value_dict = None):
+    if value_dict is None:
+        value_dict = values
+    if key == '':
+        return value_dict
+    elif '/' in key:
+        directory, setting = key.split('/',1)
+        directory += '/'
+        if directory in value_dict:
+            return get(setting, value_dict[directory])
+        else:
+            return get(setting, {})
+    else:
+        return value_dict.get(key)
+
+def _set(key, value, value_dict):
+    if '/' in key:
+        directory, setting = key.split('/',1)
+        directory += '/'
+        if directory not in value_dict:
+            value_dict[directory] = {}
+        _set(setting, value, value_dict[directory])
+    else:
+        value_dict[key] = value
 
 def set(key, value):
-    values[key] = value
+    _set(key, value, values)
     for n in notify_objects:
         if n.key == key:
             n.function(value)
