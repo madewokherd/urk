3824e357c918b6973d1a65339afdc9565d601b0a
marcldl <marcldl>
2005-09-14 01:21:04 +0000

    Remove the mysterious function I added because I didn't know better

--- urk.py
+++ urk.py
@@ -8,11 +8,8 @@ import os
 
 #add ~/.urk and path (normally where urk.py is located) to sys.path
 def path(filename):
-    return filename
-
-def path(filename):
     urkpath = os.path.dirname(inspect.getfile(sys.modules[__name__]))
-    
+
     return os.path.join(urkpath, filename)
 
 sys.path = [os.path.expanduser("~/.urk"),".",path] + sys.path
