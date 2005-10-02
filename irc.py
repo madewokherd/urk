ce29e4691e17f079bc84858e98e88055c92ae3a1
marcldl <marcldl>
2005-10-02 06:25:12 +0000

    cleanup

--- irc.py
+++ irc.py
@@ -24,11 +24,15 @@ def parse_irc(msg, server):
     else:
         msg.insert(0, server)
     
-    # loop through the msg until we find the :blah token
-    # once we do, remove it and join the rest of the tokens up as a sentence
+    # loop through the msg until we find 
+    # something beginning with :
     for i, token in enumerate(msg):
         if token.startswith(":"):
-            msg[i:] = [" ".join([msg[i][1:]] + msg[i+1:])]
+            # remove the :
+            msg[i] = msg[i][1:]
+            
+            # join up the rest
+            msg[i:] = [" ".join(msg[i:])]
             break
     
     # filter out the empty pre-":" tokens and add on the text to the end
