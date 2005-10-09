0919f5f88e2ee4663ce157772b074ac7e53eb321
marcldl <marcldl>
2005-10-09 17:57:12 +0000

    Start using scripts/ subdirs to load scripts

--- urk.py
+++ urk.py
@@ -9,34 +9,24 @@ import os
 #add ~/.urk and path (normally where urk.py is located) to sys.path
 def path(filename=""):
     urkpath = os.path.dirname(inspect.getfile(sys.modules[__name__]))
-
-    return os.path.join(urkpath, filename)
+    
+    if filename:
+        return os.path.join(urkpath, filename)
+    else:
+        return urkpath
 
 sys.path = [
-    os.path.join(os.path.expanduser("~"),".urk"),
+    os.path.expanduser("~/.urk"),
+    os.path.expanduser("~/.urk/scripts"),
     ".",
-    path()
+    "./scripts",
+    path(),
+    path("scripts")
     ] + sys.path
 
 import events
-
-opts, dummy = getopt.getopt(sys.argv[1:],'c:u:', ['conf=','ui='])
-
-opts = dict(opts)
-
-conf_module = imp.find_module(opts.get('-c') or opts.get('--conf') or 'conf')
-try:
-    conf = imp.load_module('conf', *conf_module)
-except:
-    conf_module[0].close()
-    print "Unable to load gconf; your settings will not be saved."
-    conf_module = imp.find_module('conf_dummy')
-    conf = imp.load_module('conf', *conf_module)
-conf_module[0].close()
-
-ui_module = imp.find_module(opts.get('-u') or opts.get('--ui') or 'ui')
-ui = imp.load_module('ui', *ui_module)
-ui_module[0].close()
+import conf
+import ui
 
 name = "urk"
 long_name = "urk IRC"
@@ -45,12 +35,18 @@ long_version = "%s v%s" % (long_name, ".".join(str(x) for x in version))
 website = "http://urk.sf.net/"
 authors = ["Vincent Povirk", "Marc Liddell"]
 copyright = "2005 %s" % ', '.join(authors)
-default_scripts = ['theme','chaninfo', 'ctcp', 'irc_script', 'ui_script', 'hotlinking', 'keys']
 
-if __name__ == "__main__":
-    for script in conf.get("scripts_to_load") or default_scripts:
-        events.load(script)
+def main():
+    for script_path in set(sys.path[1:6:2]):
+        try:
+            for script in os.listdir(script_path):
+                events.load(script)
+        except OSError:
+            pass
 
     events.trigger("Start")
     
     ui.start()
+
+if __name__ == "__main__":
+    main()
