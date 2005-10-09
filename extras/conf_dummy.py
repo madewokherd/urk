104bcfecf783c90d0fe206c062e2b26874bcbf64
marcldl <marcldl>
2005-10-09 17:55:04 +0000

    Extras stuff

---
+++ extras/conf_dummy.py
@@ -0,0 +1,59 @@
+#dummy conf module that doesn't save settings across sessions
+
+#this is mostly here so developers can simulate a fresh install, but users can
+#use it as a substitute for the normal conf.py, which requires gnome-python
+
+#to force urk to use this module, run 'python urk.py -c conf_dummy'
+
+#the initial settings; you should make a copy if you want to change these
+values = {
+    #'setting-name': python-expression,
+    #'setting-name': python-expression,
+    }
+
+
+import weakref
+
+notify_objects = weakref.WeakKeyDictionary()
+
+def get(key, value_dict = None):
+    if value_dict is None:
+        value_dict = values
+    if key == '':
+        return value_dict
+    elif '/' in key:
+        directory, setting = key.split('/',1)
+        directory += '/'
+        if directory in value_dict:
+            return get(setting, value_dict[directory])
+        else:
+            return get(setting, {})
+    else:
+        return value_dict.get(key)
+
+def _set(key, value, value_dict):
+    if '/' in key:
+        directory, setting = key.split('/',1)
+        directory += '/'
+        if directory not in value_dict:
+            value_dict[directory] = {}
+        _set(setting, value, value_dict[directory])
+    else:
+        value_dict[key] = value
+
+def set(key, value):
+    _set(key, value, values)
+    for n in notify_objects:
+        if n.key == key:
+            n.function(value)
+
+class notify:
+    def __init__(self, key, function):
+        self.key = key
+        self.function = function
+        notify_objects[self] = None
+    def stop(self):
+        try:
+            del notify_objects[self]
+        except KeyError:
+            return
---
+++ extras/ui_script_traditional.py
@@ -0,0 +1,100 @@
+import ui
+import chaninfo
+
+# FIXME: meh still might want rid of these, I'm not sure yet
+
+def onActive(window):
+    window.activity = 0
+        
+    if window.role != ui.StatusWindow:
+        title = "%s - %s - %s" % (window.network.me, window.network.server, window.title)
+    else:
+        title = "%s - %s" % (window.network.me, window.title)
+    
+    ui.set_title("%s - urk" % title)
+    
+    ui.register_idle(window.focus)
+
+def onNick(e):
+    if e.source == e.network.me:
+        for w in ui.get_window_for(network=e.network):
+            w.nick_label.update(e.newnick)     
+
+def onExit(e):
+    for w in ui.get_window_for(role=ui.StatusWindow):
+        w.close()
+        
+# /FIXME
+
+
+def get_status_window(network):
+    # There can be only one...
+    for window in ui.get_window_for(role=ui.StatusWindow, network=network):
+        return window
+
+ui.get_default_window = get_status_window
+
+def preJoin(e):
+    if e.source == e.network.me:
+        ui.windows.new(ui.ChannelWindow, e.network, e.target).activate()
+        
+    e.window = ui.windows.get(ui.ChannelWindow, e.network, e.target) or e.window
+
+def preText(e):
+    if e.target == e.network.me:
+        e.window = ui.windows.new(ui.QueryWindow, e.network, e.source)
+    else:
+        e.window = \
+            ui.windows.get(ui.ChannelWindow, e.network, e.target) or \
+            ui.windows.get(ui.QueryWindow, e.network, e.source) or \
+            e.window
+
+preAction = preText
+
+def preOwnText(e):
+    e.window = \
+        ui.windows.get(ui.ChannelWindow, e.network, e.target) or \
+        ui.windows.get(ui.QueryWindow, e.network, e.target) or \
+        e.window
+
+preOwnAction = preOwnText
+
+def postPart(e):
+    if e.source == e.network.me:
+        window = ui.windows.get(ui.ChannelWindow, e.network, e.target)
+        if window:
+            window.close()
+
+def onClose(window):
+    if window.role == ui.ChannelWindow and \
+            chaninfo.ischan(window.network, window.id):
+        window.network.part(window.id)
+    elif window.role == ui.StatusWindow:
+        if window.network.status:
+            window.network.quit()
+        
+        for w in ui.get_window_for(network=window.network):
+            if w is not window:
+                w.close()   
+
+def onConnect(e):
+    window = ui.get_default_window(e.network)
+    if window:
+        window.title.update()
+
+def onDisconnect(e):
+    window = ui.get_default_window(e.network)
+    if window:
+        window.title.update()
+
+def setupPart(e):
+    e.window = ui.windows.get(ui.ChannelWindow, e.network, e.target) or e.window
+
+setupTopic = setupPart
+
+def setupKick(e):
+    e.window = ui.windows.get(ui.ChannelWindow, e.network, e.channel) or e.window
+
+def setupMode(e):
+    if e.target != e.network.me:
+        e.window = ui.windows.get(ui.ChannelWindow, e.network, e.target) or e.window
