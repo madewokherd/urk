6ebbf5fd58bbcf08eabb680562f498eb1651cda2
marcldl <marcldl>
2005-11-01 05:13:25 +0000

    Logging: sort of working

--- scripts/logging.py
+++ scripts/logging.py
@@ -1,79 +1,112 @@
 import time
+import os
 
+from conf import conf
 import ui
 import chaninfo
+import __main__ as urk
 
 DATE_FORMAT_LONG = '%Y-%m-%d %H:%M:%S (%Z)'
 DATE_FORMAT = '%m-%d %H:%M'
 
-def onText(e):
-    e.network, e.window.id
+LOG_DIR = conf['log_dir'] or os.path.join(urk.userpath,'logs')
+if not os.access(LOG_DIR, os.F_OK):
+    os.mkdir(LOG_DIR)
     
-    print time.strftime(DATE_FORMAT), '<%s> %s' % (e.source, e.text)
+def log_file(network, name, new=False):
+    network = network.isupport['NETWORK']
+
+    network_dir = os.path.join(LOG_DIR, network)
+    if not os.access(network_dir, os.F_OK):
+        os.mkdir(network_dir)
+    
+    name_dir = os.path.join(LOG_DIR, network, name)    
+    if not os.access(name_dir, os.F_OK):
+        os.mkdir(name_dir)
+       
+    if new:
+        recent_log = time.strftime('%Y-%m-%d %H%M%S.log')
+    else:
+        try:
+            recent_log = sorted(os.listdir(name_dir))[-1]
+        except IndexError:
+            recent_log = time.strftime('%Y-%m-%d %H%M%S.log')
+
+    return LogFile(os.path.join(name_dir, recent_log), 'a')
+    
+class LogFile(file):
+    def write(self, text):
+        file.write(self, '%s %s%s' % (time.strftime(DATE_FORMAT), text, os.linesep))
+
+def onText(e):
+    f = log_file(e.network, e.window.id)
+    
+    f.write('<%s> %s' % (e.source, e.text))
 
 def onOwnText(e):
-    e.network, e.window.id
+    f = log_file(e.network, e.window.id)
     
-    print time.strftime(DATE_FORMAT), '<%s> %s' % (e.source, e.text)
+    f.write('<%s> %s' % (e.source, e.text))
     
 def onAction(e):
-    e.network, e.window.id
+    f = log_file(e.network, e.window.id)
     
-    print time.strftime(DATE_FORMAT), '*%s %s' % (e.source, e.text)
+    f.write('*%s %s' % (e.source, e.text))
 
 def onOwnAction(e):
-    e.network, e.window.id
+    f = log_file(e.network, e.window.id)
     
-    print time.strftime(DATE_FORMAT), '*%s %s' % (e.source, e.text)
+    f.write('*%s %s' % (e.source, e.text))
 
 def onNotice(e):
     window = ui.windows.manager.get_active()
     if window.network != e.network:
         window = ui.get_default_window(e.network)
         
-    e.network, window.id
+    f = log_file(e.network, window.id)
 
-    print time.strftime(DATE_FORMAT), '-%s- %s' % (e.source, e.text)
+    f.write('-%s- %s' % (e.source, e.text))
 
 def onOwnNotice(e):
     window = ui.windows.manager.get_active()
     if window.network != e.network:
         window = ui.get_default_window(e.network)
         
-    e.network, window.id
+    f = log_file(e.network, window.id)
 
-    print time.strftime(DATE_FORMAT), '-%s- %s' % (e.source, e.text)
+    f.write('-%s- %s' % (e.source, e.text))
 
 def onCtcp(e):
     if not e.quiet:
-        e.network, e.source
+        f = log_file(e.network, e.source)
         
-        print time.strftime(DATE_FORMAT), '[%s] %s' % (e.source, e.text)
+        f.write('[%s] %s' % (e.source, e.text))
 
 def onCtcpReply(e):
     window = ui.windows.manager.get_active()
     if window.network != e.network:
         window = ui.get_default_window(e.network)
         
-    e.network, window.id
+    f = log_file(e.network, window.id)
 
-    print time.strftime(DATE_FORMAT), '--- %s reply from %s: %s' % (e.name.capitalize(), e.source, ' '.join(e.args))
+    f.write('--- %s reply from %s: %s' % (e.name.capitalize(), e.source, ' '.join(e.args)))
 
 def onJoin(e):
-    e.network, e.window.id
-
-    if e.network.me == e.source:
+    if e.network.me == e.source:   
         # START A NEW LOG FILE HERE OMG
-        print e.network.me, time.strftime(DATE_FORMAT_LONG)
-        to_write = time.strftime(DATE_FORMAT), 'You joined %s' % e.target
+        f = log_file(e.network, e.window.id, new=True)
+        f.write('%s %s' % (e.network.me, time.strftime(DATE_FORMAT_LONG)))
+
+        to_write = 'You joined %s' % e.target
 
     else:
+        f = log_file(e.network, e.window.id)
         to_write = '\x02%s\x02 (%s) joined %s' % (e.source, e.address, e.target)
         
-    print time.strftime(DATE_FORMAT), to_write
+    f.write(to_write)
 
 def onPart(e):
-    e.network, e.window.id
+    f = log_file(e.network, e.window.id)
 
     if e.network.me == e.source:
         to_write = 'You left %s' % e.target
@@ -83,17 +116,17 @@ def onPart(e):
     if e.text:
         to_write += ' (%s)' % e.text
     
-    print time.strftime(DATE_FORMAT), to_write
+    f.write(to_write)
 
 def onKick(e):
-    e.network, e.window.id
+    f = log_file(e.network, e.window.id)
 
-    print time.strftime(DATE_FORMAT), '%s kicked %s (%s)' % (e.source, e.target, e.text)
+    f.write('%s kicked %s (%s)' % (e.source, e.target, e.text))
         
 def onMode(e):
-    e.network, e.window.id
+    f = log_file(e.network, e.window.id)
     
-    print time.strftime(DATE_FORMAT), '%s sets mode: %s' % (e.source, e.text)
+    f.write('%s sets mode: %s' % (e.source, e.text))
         
 def onQuit(e):
     to_write = "%s quit (%s)" % (e.source, e.text)
@@ -102,16 +135,16 @@ def onQuit(e):
         if chaninfo.ison(e.network, channame, e.source):
             window = ui.windows.get(ui.ChannelWindow, e.network, channame)
             if window:
-                e.network, window.id
+                f = log_file(e.network, window.id)
                 
-                print time.strftime(DATE_FORMAT), to_write
+                f.write(to_write)
 
 def onNick(e):
     if e.source == e.network.me:
         to_write = 'You are now known as %s' % e.newnick
     
         for window in ui.get_window_for(network=e.network):
-            print time.strftime(DATE_FORMAT), to_write
+            f.write(to_write)
     else:
         to_write = '%s is now known as %s' % (e.source, e.newnick)
     
@@ -119,14 +152,14 @@ def onNick(e):
             if chaninfo.ison(e.network,channame,e.source):
                 window = ui.windows.get(ui.ChannelWindow, e.network, channame)
                 if window:
-                    e.network, window.id
+                    f = log_file(e.network, window.id)
                     
-                    print time.strftime(DATE_FORMAT), to_write
+                    f.write(to_write)
 
 def onTopic(e):
-    e.network, e.window.id
+    f = log_file(e.network, e.window.id)
     
-    print time.strftime(DATE_FORMAT), '%s set topic on %s: %s' % (e.source, e.target, e.text)
+    f.write('%s set topic on %s: %s' % (e.source, e.target, e.text))
 
 def onRaw(e):
     if not e.quiet:
@@ -134,29 +167,29 @@ def onRaw(e):
             if e.msg[1] == '332':
                 window = ui.windows.get(ui.ChannelWindow, e.network, e.msg[3]) or e.window
                 
-                e.network, window.id
+                f = log_file(e.network, window.id)
                 
-                print time.strftime(DATE_FORMAT), 'topic on %s is: %s' % (e.msg[3], e.text)
+                f.write('topic on %s is: %s' % (e.msg[3], e.text))
                 
             elif e.msg[1] == '333':
                 window = ui.windows.get(ui.ChannelWindow, e.network, e.msg[3]) or e.window
                 
-                e.network, window.id
+                f = log_file(e.network, window.id)
                 
-                print time.strftime(DATE_FORMAT), 'topic on %s set by %s at time %s' % (e.msg[3], e.msg[4], time.ctime(int(e.msg[5])))
+                f.write('topic on %s set by %s at time %s' % (e.msg[3], e.msg[4], time.ctime(int(e.msg[5]))))
             
             elif e.msg[1] == '329': #RPL_CREATIONTIME
                 pass
             
             else:
-                e.network, e.window.id
+                f = log_file(e.network, e.window.id)
             
-                print time.strftime(DATE_FORMAT), '* %s' % ' '.join(e.msg[3:])
+                f.write('* %s' % ' '.join(e.msg[3:]))
 
         elif e.msg[1] == 'ERROR':
-            e.network, e.window.id
+            f = log_file(e.network, e.window.id)
             
-            print time.strftime(DATE_FORMAT), 'Error: %s' % e.text
+            f.write('Error: %s' % e.text)
 
 def onDisconnect(e):
     if e.error:
@@ -165,6 +198,6 @@ def onDisconnect(e):
         to_write = '* Disconnected'
 
     for window in ui.get_window_for(network=e.network):
-        e.network, window.id
+        f = log_file(e.network, window.id)
         
-        print time.strftime(DATE_FORMAT), to_write
+        f.write(to_write)
