ec37008d0ca75b8366923ab78ebbaf24bc9977b4
marcldl <marcldl>
2005-11-03 16:02:14 +0000

    Pretty print urk.conf

--- conf.py
+++ conf.py
@@ -1,20 +1,23 @@
+import pprint
+
 import events
 
 import __main__ as urk
 import os
 
-__CONF_FILE = os.path.join(urk.userpath,'urk.conf')
+CONF_FILE = os.path.join(urk.userpath,'urk.conf')
 
-def __save(e):
-    file(__CONF_FILE, "w").write(repr(conf))
-events.register('Exit', 'post', __save)
+def save(e):
+    pprint.pprint(conf, file(CONF_FILE, "w"))
 
-class __confdict(dict):
+events.register('Exit', 'post', save)
+
+class confdict(dict):
     def __getitem__(self, key):
         if key in self:
             return dict.__getitem__(self, key)
 
-if os.access(__CONF_FILE,os.R_OK):
-    conf = __confdict(**eval(file(__CONF_FILE).read()))
+if os.access(CONF_FILE,os.R_OK):
+    conf = confdict(**eval(file(CONF_FILE).read()))
 else:
-    conf = __confdict()
+    conf = confdict()
