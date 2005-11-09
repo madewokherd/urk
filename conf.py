3cecb735a850f78c3ed85d5baa0a5439a8ecbdfb
marcldl <marcldl>
2005-11-09 17:06:37 +0000

    Possible nicer pretty print

--- conf.py
+++ conf.py
@@ -1,5 +1,3 @@
-import pprint
-
 import events
 
 import __main__ as urk
@@ -7,8 +5,42 @@ import os
 
 CONF_FILE = os.path.join(urk.userpath,'urk.conf')
 
+def pprint(x, depth=-2):
+    depth += 2
+    
+    s_list = []
+
+    if isinstance(x, dict):
+        s_list = s_list + ['{', '\n']
+    
+        for key in x:
+            s_list += ['%s%s%s' % (' '*depth, repr(key), ':')]
+            s_list += pprint(x[key], depth)
+            
+        s_list += ['%s%s' % (' '*depth, '}')]
+        s_list += [',\n']
+        
+    elif isinstance(x, list):
+        s_list += ['[', '\n']
+    
+        for item in x:
+            s_list += ['%s' % (' '*depth)]
+            s_list += pprint(item, depth)
+            
+        s_list += ['%s%s' % (' '*depth, ']')]
+        s_list += [',\n']
+        
+    else:
+        s_list += ['%s,' % repr(x), '\n']
+        
+    if depth == 0:
+        return ''.join(s_list[:-1])
+    
+    else:
+        return s_list
+
 def save(e):
-    pprint.pprint(conf, file(CONF_FILE, "w"))
+    file(CONF_FILE, "w").write(pprint(conf))
 
 events.register('Exit', 'post', save)
 
