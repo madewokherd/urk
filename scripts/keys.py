3fcc5d6dc2da7d099a1437352e1c0eb2f7fa4134
marcldl <marcldl>
2005-11-09 16:58:28 +0000

    Some nice tab navigation

--- scripts/keys.py
+++ scripts/keys.py
@@ -19,5 +19,23 @@ def onKeyPressed(e):
         if w:
             ui.windows.manager.set_active(w[0])
 
+    # tabbed browsing
     elif e.key == '^t':
         ui.windows.new(ui.StatusWindow, None, 'status').activate()
+
+    elif e.key == '^w':
+        ui.windows.manager.get_active().close()
+        
+    elif e.key == '^Left':
+        w = list(ui.windows.manager)
+    
+        i = w.index(ui.windows.manager.get_active())
+    
+        ui.windows.manager.set_active(w[i-1])
+        
+    elif e.key == '^Right':
+        w = list(ui.windows.manager)
+    
+        i = w.index(ui.windows.manager.get_active())
+    
+        ui.windows.manager.set_active(w[(i+1) % len(w)])
