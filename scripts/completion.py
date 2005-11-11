7eb3fcdaf81a6bd168f7fae89f56f787dc7bd4c0
marcldl <marcldl>
2005-11-11 00:38:02 +0000

    Slightly different approach

--- scripts/completion.py
+++ scripts/completion.py
@@ -3,101 +3,57 @@ import chaninfo
 import events
 
 def channel_completer(window, left, right, text):
-    network, input = window.network, window.input
-    
-    if text:
-        insert_text = "%s%s%s" % (left[:-len(text)], "%s", right)
-        cursor_pos = len(left[:-len(text)])
-    else:
-        insert_text = "%s%s%s" % (left, "%s", right)
-        cursor_pos = len(left)
-
-    candidates = [w.id for w in ui.windows.manager if w.role == ui.ChannelWindow]
-
-    result = []       
-    for channel in candidates:
-        if channel.lower().startswith(text.lower()):
-            result.append((insert_text % channel, cursor_pos + len(channel)))
-                
-    result.append((input.text, input.cursor))
-            
-    while True:
-        for text, cursor in result:
-            input.text, input.cursor = text, cursor
-            yield None
+    return [w.id for w in ui.windows.manager if w.role == ui.ChannelWindow]
             
 # normal server commands
 srv_commands = ['PING', 'JOIN', 'PART', 'MODE', 'SERVER', 'KICK',
                 'QUIT', 'NICK', 'PRIVMSG', 'NOTICE', 'TOPIC']
             
 def command_completer(window, left, right, text):
-    network, input = window.network, window.input
-    
-    if text:
-        insert_text = "%s%s%s" % (left[:-len(text)], "%s", right)
-        cursor_pos = len(left[:-len(text)])
-    else:
-        insert_text = "%s%s%s" % (left, "%s", right)
-        cursor_pos = len(left)
-
     candidates = [c.lower() for c in srv_commands]
     candidates += [e[7:].lower() for e in events.events if e.startswith('Command')]
-    
-    candidates = set('/%s' % c for c in candidates if c)
 
-    result = []
-    for command in candidates:
-        if command.lower().startswith(text.lower()):
-            result.append((insert_text % command, cursor_pos + len(command)))
-                
-    result.append((input.text, input.cursor))
-            
-    while True:
-        for text, cursor in result:
-            input.text, input.cursor = text, cursor
-            yield None
+    return set('/%s' % c for c in candidates if c)
 
 def nick_completer(window, left, right, text):  
-    network, input = window.network, window.input
- 
-    if left == text:
-        suffix = ': '
-    else:
-        suffix = ' '
-    
-    if text:
-        insert_text = "%s%s%s%s" % (left[:-len(text)], "%s", suffix, right)
-        cursor_pos = len(left[:-len(text)] + suffix)
-    else:
-        insert_text = "%s%s%s%s" % (left, "%s", suffix, right)
-        cursor_pos = len(left + suffix)
+    network = window.network
 
     candidates = list(recent_speakers.get(window, []))
     candidates += [n for n in chaninfo.nicks(network, window.id) if n not in candidates]
-    
-    candidates = [n for n in candidates if chaninfo.ison(network, window.id, n)]
 
+    return [n for n in candidates if chaninfo.ison(network, window.id, n)]
+
+def get_completer_for(window, left, right, text):
+    if text and text[0] in window.network.isupport.get('CHANTYPES', '#&+'):
+        candidates = channel_completer(window, left, right, text)
+        suffix = ''
+        
+    elif text.startswith('/'):
+        candidates = command_completer(window, left, right, text)
+        suffix = ''
+        
+    else:
+        candidates = nick_completer(window, left, right, text)
+        
+        if left == text:
+            suffix = ': '
+        else:
+            suffix = ' '
+            
+    insert_text = "%s%s%s%s" % (left[:-len(text)], "%s", suffix, right)
+    cursor_pos = len(left[:-len(text)] + suffix)
+        
     result = []       
-    for nick in candidates:
-        if nick.lower().startswith(text.lower()):
-            result.append((insert_text % nick, cursor_pos + len(nick)))
+    for res in candidates:
+        if res.lower().startswith(text.lower()):
+            result += [(insert_text % res, cursor_pos + len(res))]
                 
-    result.append((input.text, input.cursor))
+    result += [(window.input.text, window.input.cursor)]
             
     while True:
         for text, cursor in result:
-            input.text, input.cursor = text, cursor
+            window.input.text, window.input.cursor = text, cursor
             yield None
-
-def get_completer_for(window, text):
-    if text and text[0] in window.network.isupport.get('CHANTYPES', '#&+'):
-        return channel_completer
-        
-    elif text.startswith('/'):
-        return command_completer
-        
-    else:
-        return nick_completer
      
 # generator--use recent_completer.next() to continue cycling through whatever
 recent_completer = None
@@ -116,7 +72,7 @@ def onKeyPress(e):
                 
                 text = left.split(' ')[-1]
                 
-                recent_completer = get_completer_for(e.window, text)(e.window, left, right, text)
+                recent_completer = get_completer_for(e.window, left, right, text)
 
             recent_completer.next()
     
