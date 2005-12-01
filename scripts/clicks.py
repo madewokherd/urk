41a5024e86e50d3dcca22597da0c4642751667ae
marcldl <marcldl>
2005-12-01 15:52:11 +0000

    Combine clicks and hotlinking

--- scripts/clicks.py
+++ scripts/clicks.py
@@ -3,7 +3,45 @@ import ui
 import chaninfo
 from conf import conf
 
-def is_nick(e, target):
+def set_target(e):
+    target_l = e.target.lstrip('@+%.(<')
+    e._target_fr = e.target_fr + len(e.target) - len(target_l)
+    
+    target_r = e.target.rstrip(')>:,')
+    e._target_to = e.target_to - len(e.target) + len(target_r)
+    
+    e._target = e.text[e._target_fr:e._target_to]
+
+def is_nick(e):
+    return e.window.role == ui.ChannelWindow and \
+            chaninfo.ison(e.window.network, e.window.id, e._target)
+    
+def is_url(e):
+    def starts(prefix, mindots=1):
+        def prefix_url(target):
+            return target.startswith(prefix) and target.count('.') >= mindots
+            
+        return prefix_url
+    
+    to_check = [starts(*x) for x in [
+        ('http://', 1),
+        ('https://', 1),
+        ('ftp://', 1),
+        ('www', 2),
+        ]]
+
+    for check_url in to_check:
+        if check_url(e._target):
+            return True
+    
+    return False
+    
+def is_chan(e):
+    # click on a #channel
+    return e.window.network and \
+            e._target[0:1] in (e.window.network.isupport.get('CHANTYPES', '&#$+'))
+            
+def make_nick_menu(e, target):
     def query():
         events.run('query %s' % target, e.window, e.window.network)
     
@@ -11,73 +49,79 @@ def is_nick(e, target):
         events.run('whois %s' % target, e.window, e.window.network)
     
     e.menu += [
-        ("Query", query),
-        ("Whois", whois),
+        ('Query', query),
+        ('Whois', whois),
         (),
         ]
-    
-def is_url(e, target):
-    def copy_to():
-        # copy to clipboard
-        ui.set_clipboard(target)
-        
-    e.menu.append(('Copy', copy_to))
-    
-def is_chan(e, target):
-    def add_to_perform():
-        if 'networks' not in conf:
-            conf['networks'] = {}
-        networks = conf['networks']
-        if e.window.network.name in networks:
-            network = networks[e.window.network.name]
-        else:
-            network_name = e.window.network.isupport.get('NETWORK',e.network.server)
-            while network_name in networks:
-                network_name += '_'
-            network = networks[network_name] = {}
-        for i, command in enumerate(network.get('perform',())):
-            # this might be worth updating to account for channel keys
-            if command.lower().startswith('join ') and command.count(' ') == 1:
-                network['perform'][i] += ','+target
-                break
-        else:
-            network['perform'] = network.get('perform',[])+('join %s' % target)
-        
-    def remove_from_perform():
-        # remove if it's already there maybe
-        pass
-        
-    #e.menu.append(('Join channel automatically', add_to_perform))
 
-def onRightClick(e):
-    # nick on this channel
-    target_l = e.target.lstrip('@+%.(<')
-    target_fr = e.target_fr + len(e.target) - len(target_l)
-    
-    target_r = e.target.rstrip(')>:,')
-    target_to = e.target_to - len(e.target) + len(target_r)
-    
-    target = e.text[target_fr:target_to]
-    
-    if e.window.role == ui.ChannelWindow and \
-            chaninfo.ison(e.window.network, e.window.id, target):
+def onHover(e):
+    set_target(e)
 
-        is_nick(e, target)
+    for is_check in (is_nick, is_url, is_chan):
+        if is_check(e):
+            e.tolink.add((e._target_fr, e._target_to))
+            break
 
+def onClick(e):
+    set_target(e)
+
+    if is_nick(e):
+        events.run('query %s' % e._target, e.window, e.window.network)
+    
     # url of the form http://xxx.xxx or www.xxx.xxx       
-    elif (target.startswith("http://") and target.count(".") >= 1) or \
-         (target.startswith("https://") and target.count(".") >= 1) or \
-         (target.startswith("www") and target.count(".") >= 2):
-        if target.startswith("www"):
-            target = "http://"+target
-        
-        is_url(e, target)
+    elif is_url(e):
+        if e._target.startswith('www'):
+            e._target = 'http://%s' % e._target
+        ui.open_file(e._target)
     
     # click on a #channel
-    elif e.window.network and \
-            target[0:1] in (e.window.network.isupport.get('CHANTYPES') or '&#$+'):
+    elif is_chan(e):
+        if not chaninfo.ischan(e.window.network, e._target):
+            e.window.network.join(e._target)
+        window = ui.windows.get(ui.ChannelWindow, e.network, e._target)
+        if window:
+            window.activate()
+
+def onRightClick(e):
+    set_target(e)
+
+    # nick on this channel
+    if is_nick(e):
+        make_nick_menu(e, e._target)
+
+    elif is_url(e):
+        if e._target.startswith('www'):
+            e._target = 'http://%s' % e._target
+    
+        def copy_to():
+            # copy to clipboard
+            ui.set_clipboard(e._target)
+            
+        e.menu += [('Copy', copy_to)]
         
-        is_chan(e, target)
+    elif is_chan(e):
+        def add_to_perform():
+            if 'networks' not in conf:
+                conf['networks'] = {}
+            networks = conf['networks']
+            if e.window.network.name not in networks:
+                networks[e.window.network.name] = {}
+            
+            network = networks[e.window.network.name]
+
+            for i, command in enumerate(network.get('perform',())):
+                # this might be worth updating to account for channel keys
+                if command.lower().startswith('join ') and command.count(' ') == 1:
+                    network['perform'][i] += ',%s' % e._target
+                    break
+            else:
+                network['perform'] = network.get('perform',[])+('join %s' % e._target)
+            
+        def remove_from_perform():
+            # remove if it's already there maybe
+            pass
+            
+        #e.menu.append(('Join channel automatically', add_to_perform))
 
 def onListRightClick(e):
-    is_nick(e, e.nick)
+    make_nick_menu(e, e.nick)
