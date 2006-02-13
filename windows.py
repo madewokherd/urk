fd7b0f03df22f4bc16d0cbed33cec7b122f804c9
marcldl <marcldl>
2006-02-13 01:43:48 +0000

    Give nicklists a toggle ability

--- windows.py
+++ windows.py
@@ -24,17 +24,15 @@ def get_default_transfer_text(self):
 
 def StatusWindow(self):    
     if hasattr(self, "output"):
-        parent = self.output.get_property("parent")
-        if parent:
-            parent.remove(self.output)
+        if self.output.parent:
+            self.output.parent.remove(self.output)
         
     else:
         self.output = ui.widgets.TextOutput(self)
-
+        
     if hasattr(self, "input"):
-        parent = self.input.get_property("parent")
-        if parent:
-            parent.remove(self.input)
+        if self.input.parent:
+            self.input.parent.remove(self.input)
 
     else:
         self.input = ui.widgets.TextInput(self)
@@ -76,17 +74,15 @@ def QueryWindow(self):
 
 def ChannelWindow(self):
     if hasattr(self, "output"):
-        parent = self.output.get_property("parent")
-        if parent:
-            parent.remove(self.output)
+        if self.output.parent:
+            self.output.parent.remove(self.output)
         
     else:
         self.output = ui.widgets.TextOutput(self)
         
     if hasattr(self, "input"):
-        parent = self.input.get_property("parent")
-        if parent:
-            parent.remove(self.input)
+        if self.input.parent:
+            self.input.parent.remove(self.input)
 
     else:
         self.input = ui.widgets.TextInput(self)
@@ -108,24 +104,45 @@ def ChannelWindow(self):
     topbox.add(self.output)
     
     nlbox = gtk.ScrolledWindow()
-    nlbox.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)   
-    nlbox.add(self.nicklist.view)
-    
-    nlbox.set_size_request(conf.get("ui-gtk/nicklist-width",0), -1)
+    nlbox.set_policy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)   
+    nlbox.add(self.nicklist)
 
-    def save_nicklist_width(w, rectangle):
-        conf["ui-gtk/nicklist-width"] = rectangle.width
-    nlbox.connect("size-allocate", save_nicklist_width)
-    
+    nlbox.set_size_request(conf.get("ui-gtk/nicklist-width", 50), -1)
+
     botbox = gtk.HBox()
     botbox.pack_start(self.input)
     botbox.pack_end(self.nick_label, expand=False)
     
     self.pack_end(botbox, expand=False)
     
-    pane = gtk.HPaned()  
+    pane = gtk.HPaned()
     pane.pack1(topbox, resize=True, shrink=False)
     pane.pack2(nlbox, resize=False, shrink=True)
+    
+    nl_pos = [None]    
+    def move_nicklist(paned, event):
+        nl_pos[0] = paned.get_position()
+    
+    def drop_nicklist(paned, event):   
+        width = paned.allocation.width
+        pos = paned.get_position()
+        
+        if pos == nl_pos[0]:
+            if width - pos <= 10:
+                conf_nicklist = conf.get("ui-gtk/nicklist-width", 200)
+
+                if conf_nicklist <= 10:
+                    paned.set_position(width - 200)
+                else:
+                    paned.set_position(width - conf_nicklist)
+            else:
+                paned.set_position(width)
+                
+        else:
+            conf["ui-gtk/nicklist-width"] = width - pos
+        
+    pane.connect("button-press-event", move_nicklist)
+    pane.connect("button-release-event", drop_nicklist)
     
     self.pack_end(pane)
 
