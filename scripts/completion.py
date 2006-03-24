27d001d79241d1193ffc6becf64d11a2d8ed4a08
marcldl <marcldl>
2006-03-24 17:51:26 +0000

    Make completion reset every cycle round

--- scripts/completion.py
+++ scripts/completion.py
@@ -16,10 +16,12 @@ def command_completer(window, left, right, text):
         
     for c in events.events:
         if c.startswith('Command') and c != 'Command':
-            yield '/%s' % [7:].lower()
+            yield '/%s' % c[7:].lower()
 
 def nick_completer(window, left, right, text):  
     recent_speakers = getattr(window, 'recent_speakers', ())
+    
+    
     
     for nick in recent_speakers:
         if chaninfo.ison(window.network, window.id, nick):
@@ -41,51 +43,50 @@ def get_completer_for(window):
     left, right = input.text[:input.cursor], input.text[input.cursor:]
             
     text = left.split(' ')[-1]
-    fulltext = 
 
-    suffix = ''
-
-    if text and text[0] in window.network.isupport.get('CHANTYPES', '#&+'):
-        candidates = channel_completer(window, left, right, text)
+    while True:
+        suffix = ''
+        if text and text[0] in window.network.isupport.get('CHANTYPES', '#&+'):
+            candidates = channel_completer(window, left, right, text)
+            
+        elif input.text.startswith('/reload '):
+            candidates = script_completer(window, left, right, text)
         
-    elif input.text.startswith('/reload '):
-        candidates = script_completer(window, left, right, text)
-    
-    elif input.text.startswith('/edit '):
-        candidates = script_completer(window, left, right, text)
-        
-    elif input.text.startswith('/server '):
-        candidates = network_completer(window, left, right, text)
-        
-    elif input.text.startswith('/'):
-        candidates = command_completer(window, left, right, text)
-        suffix = ' '
-        
-    else:
-        candidates = nick_completer(window, left, right, text)
-        
-        if left == text:
-            suffix = ': '
-        else:
+        elif input.text.startswith('/edit '):
+            candidates = script_completer(window, left, right, text)
+            
+        elif input.text.startswith('/server '):
+            candidates = network_completer(window, left, right, text)
+            
+        elif input.text.startswith('/'):
+            candidates = command_completer(window, left, right, text)
             suffix = ' '
             
-    if text:
-        before = left[:-len(text)]
-    else:
-        before = left
-        
-    insert_text = '%s%s%s%s' % (before, '%s', suffix, right)
-    cursor_pos = len(before + suffix)
-
-    original = window.input.text, window.input.cursor
+        else:
+            candidates = nick_completer(window, left, right, text)
             
-    while True:
+            if left == text:
+                suffix = ': '
+            else:
+                suffix = ' '
+                
+        if text:
+            before = left[:-len(text)]
+        else:
+            before = left
+            
+        insert_text = '%s%s%s%s' % (before, '%s', suffix, right)
+        cursor_pos = len(before + suffix)
+
+        original = window.input.text, window.input.cursor
+
         for cand in candidates:
             if cand.lower().startswith(text.lower()):
-                window.input.text, window.input.cursor = insert_text % res, cursor_pos + len(res)
+                window.input.text, window.input.cursor = insert_text % cand, cursor_pos + len(cand)
                 yield None
                 
         window.input.text, window.input.cursor = original
+        yield None
      
 # generator--use recent_completer.next() to continue cycling through whatever
 recent_completer = None
