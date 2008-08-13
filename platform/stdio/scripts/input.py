b93bb4c82be28e26c4acbda8f12e66fdbe2e73f9
Vincent Povirk <madewokherd+d41d@gmail.com>
2008-08-13 12:28:37 -0500

    stdio: add /w command to switch to a different window

--- platform/stdio/scripts/input.py
+++ platform/stdio/scripts/input.py
@@ -1,6 +1,8 @@
 import ui
 import windows
 
+active_window = None
+
 def handle_input(result, error):
     if result is not None:
         windows.manager.get_active().entered_text(result)
@@ -9,6 +11,21 @@ def handle_input(result, error):
         return
 
     ui.fork(handle_input, raw_input)
+
+def onActive(e):
+    global active_window
+    if active_window != e.window.id:
+        active_window = e.window.id
+        print "Active window is now %s" % e.window.id
+
+def onCommandW(e):
+    if len(e.args) >= 1:
+        for window in windows.manager:
+            if window.id == e.args[0]:
+                window.activate()
+                break
+    else:
+        print "Active window is now %s" % windows.get_active().id
 
 ui.fork(handle_input, raw_input)
 
